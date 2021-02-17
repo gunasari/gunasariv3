dde21d578066110da887705906a8faeb8fe56703
gunasari <78573985+gunasari@users.noreply.github.com>
2021-02-17 13:49:40 +0700

    menghapus bagian selain profil desa

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@
 $(document).ready(function() {
     $("#myCarousel").carousel("cycle");
 })
+
+// collapsible
+var coll = document.getElementById("button");
+var content = document.getElementById("profil-lengkap")
+coll.addEventListener("click", function(){
+    if(content.style.display === "block"){
+        content.style.display = "none";
+        coll.innerHTML = "Lihat Selengkapnya...";
+    } else {
+        content.style.display = "block";
+        coll.innerHTML = "Kembali";
+    }
+});
