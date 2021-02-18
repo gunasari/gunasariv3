474d38d3f6b23e7608d89e847cca686a3daf9f2f
gunasari <78573985+gunasari@users.noreply.github.com>
2021-02-18 13:41:27 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,9 +10,11 @@ var content = document.getElementById("profil-lengkap")
 coll.addEventListener("click", function(){
     if(content.style.display === "block"){
         content.style.display = "none";
+        content.style.maxHeight = null;
         coll.innerHTML = "Lihat Selengkapnya...";
     } else {
         content.style.display = "block";
+        content.style.maxHeight = content.scrollHeight + "px";
         coll.innerHTML = "Kembali";
     }
 });
